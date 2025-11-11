706007db7a78dc2999f27fbed5e88f5e2cea39be
averel10 <achille.huenenberger@gmail.com>
2025-11-11 20:15:26 +0100

    fix: update image source paths in Home component and remove assetPrefix from Next.js config

--- crypto_clash_frontend/next.config.ts
+++ crypto_clash_frontend/next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   basePath: "/crypto_clash",
-  assetPrefix: "/crypto_clash",
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
 };
