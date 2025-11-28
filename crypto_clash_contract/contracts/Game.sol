cccd3b093e8780aae9decc10b5284932f7381d30
SamKry <87971518+SamKry@users.noreply.github.com>
2025-11-28 15:55:40 +0100

    revert the second require

--- crypto_clash_contract/contracts/Game.sol
+++ crypto_clash_contract/contracts/Game.sol
@@ -57,7 +57,7 @@ contract Game {
         require(msg.value >= BET_MIN, "Minimum bet not met");
         require(
             games[gameId].initialBet == 0 ||
-                msg.value >= games[gameId].initialBet,
+                msg.value == games[gameId].initialBet,
             "Bet value must match initial bet"
         );
         _;
